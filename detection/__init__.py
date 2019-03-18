2484fd367162f299b851b6e71629d16498bcb59f
Unknown <k.naivete.5656@gmail.com>
2019-03-18 11:23:49 +0900

    changefilekaisou

--- detection/__init__.py
+++ detection/__init__.py
@@ -1,3 +1,4 @@
 from .detection_train import TrainNet
+from .merge_net_train import TrainMarge
 from .train_second import TrainNetSecond
 from .predict import Predict, PredictFmeasure
--- detection/merge_net_train.py
+++ detection/merge_net_train.py
@@ -1,8 +1,6 @@
 import torch
 from tqdm import tqdm
 from utils import *
-import os
-os.chdir(Path.cwd().parent)
 from datetime import datetime
 from networks import UNet, MargeNet
 from detection import TrainNet
@@ -84,39 +82,39 @@ class TrainMarge(TrainNet):
         self.show_graph()
 
 
-if __name__ == "__main__":
-
-    torch.cuda.set_device(1)
-    # mode = "single"
-    mode = "single"
-    plot_size = 3
-    norm = True
-    date = datetime.now().date()
-    train_path = Path("./images/C2C12P7/sequ_cut/0318/sequ17")
-    val_path = Path("./images/C2C12P7/sequ_cut/0318/sequ18")
-    save_weight_path = Path("/home/kazuya/file_server2/weights/marge/best_{}.pth".format(plot_size))
-    save_path = Path("./detection/confirm")
-    pre_trained_path = './weights/MSELoss/best_12.pth'
-    # pre_trained_path = ''
-    models = {"single": MargeNet}
-    net = UNet(n_channels=1, n_classes=1, sig=norm)
-    if pre_trained_path is not None:
-        net.load_state_dict(torch.load(pre_trained_path, map_location={"cuda:3": "cuda:1"}))
-    net = MargeNet(n_channels=1, n_classes=1, sig=norm, net=net)
-
-    net.cuda()
-
-    train = TrainMarge(
-        net=net,
-        mode=mode,
-        epochs=500,
-        batch_size=5,
-        lr=1e-5,
-        gpu=True,
-        plot_size=plot_size,
-        train_path=train_path,
-        val_path=val_path,
-        weight_path=save_weight_path,
-    )
-
-    train.main()
+# if __name__ == "__main__":
+#
+#     torch.cuda.set_device(1)
+#     # mode = "single"
+#     mode = "single"
+#     plot_size = 3
+#     norm = True
+#     date = datetime.now().date()
+#     train_path = Path("./images/C2C12P7/sequ_cut/0318/sequ17")
+#     val_path = Path("./images/C2C12P7/sequ_cut/0318/sequ18")
+#     save_weight_path = Path("/home/kazuya/file_server2/weights/marge/best_{}.pth".format(plot_size))
+#     save_path = Path("./detection/confirm")
+#     pre_trained_path = './weights/MSELoss/best_12.pth'
+#     # pre_trained_path = ''
+#     models = {"single": MargeNet}
+#     net = UNet(n_channels=1, n_classes=1, sig=norm)
+#     if pre_trained_path is not None:
+#         net.load_state_dict(torch.load(pre_trained_path, map_location={"cuda:3": "cuda:1"}))
+#     net = MargeNet(n_channels=1, n_classes=1, sig=norm, net=net)
+#
+#     net.cuda()
+#
+#     train = TrainMarge(
+#         net=net,
+#         mode=mode,
+#         epochs=500,
+#         batch_size=5,
+#         lr=1e-5,
+#         gpu=True,
+#         plot_size=plot_size,
+#         train_path=train_path,
+#         val_path=val_path,
+#         weight_path=save_weight_path,
+#     )
+#
+#     train.main()
